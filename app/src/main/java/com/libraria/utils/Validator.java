b9da161ab350ac26b77e9f2d1ab01c10843ba8fa
Patriciusss <patriciusdanduru@gmail.com>
2026-05-26 10:30:57 +0800

    feat: menambahkan Validator untuk validasi input

--- app/src/main/java/com/libraria/utils/Validator.java
+++ app/src/main/java/com/libraria/utils/Validator.java
@@ -1,5 +1,27 @@
 package com.libraria.utils;
 
 public class Validator {
-    
+    public boolean isFieldNotEmpty(String text) {
+        if (text.isEmpty()) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    public boolean isPasswordMatch(String newPassword, String confirmPassword) {
+        if (oldPassword.equals(newPassword)) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean isPasswordValid(String password) {
+        if (password.length() >= 8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
